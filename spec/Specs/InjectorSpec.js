266035b9e7f87c37baba23efbfe8e1361f600b01
Franklin Paula <frankpaul142@gmail.com>
2017-10-15 15:48:08 -0500

    test fix

--- spec/Specs/InjectorSpec.js
+++ spec/Specs/InjectorSpec.js
@@ -14,11 +14,14 @@ describe("Injector", function() {
     var speed=100;
     injector.initialize(0);
     expect(injector).toBeRunning(speed);
+    console.log('injector running');
   });
   it("the injector it's damaged", function() {
     var speed=100;
     var damage=getRandomInt(1,100);
-    injector.initialize(20);
+    injector.initialize(damage);
     expect(injector).toBeDamaged();
+    console.log('injector damaged Capacity:');
+    console.log(injector.getCapacity());
   });
 });
