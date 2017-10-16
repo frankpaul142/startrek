88fdfa20b197d54f92e2d73ec78bf0c6ee1e6507
Franklin Paula <frankpaul142@gmail.com>
2017-10-15 23:26:49 -0500

    fun tweaks

--- index.js
+++ index.js
@@ -8,22 +8,25 @@ rl.on('line', function(line) {
   switch(line.trim()) {
     case 'hello':
     case 'help':
-      console.log('Hello, type speed, damageA,damageB,damageC on this format: speed damageA damageB damageC');
-      console.log(prefix + 'Example: 100 0 0 0');
+      console.log('Example> 100 0 0 0');
       break;
+    case 'exit':
+      console.log('Have a great day saving the galaxy!');
+      process.exit(0);
+    break;
     default:
     	var inputs=line.split(' ');
-		var speed=inputs[0];
+		  var speed=inputs[0];
 	    injectorA = new Injector();
 	    injectorB = new Injector();
 	    injectorC = new Injector();
 	    reactor = new Reactor();
-		injectorA.initialize(inputs[1]);
-		injectorB.initialize(inputs[2]);
-		injectorC.initialize(inputs[3]);
-		reactor.initialize(speed,injectorA,injectorB,injectorC);
-		reactor.SpeedUp();
-      break;
+  		injectorA.initialize(inputs[1]);
+  		injectorB.initialize(inputs[2]);
+  		injectorC.initialize(inputs[3]);
+  		reactor.initialize(speed,injectorA,injectorB,injectorC);
+  		reactor.SpeedUp();
+    break;
   }
   rl.setPrompt(prefix, prefix.length);
   rl.prompt();
@@ -32,6 +35,6 @@ rl.on('line', function(line) {
   process.exit(0);
 });
 console.log(prefix + 'Hello, type speed, damageA,damageB,damageC on this format: speed damageA damageB damageC');
-console.log(prefix + 'Example: 100 0 0 0');
+console.log('Example> 100 0 0 0');
 rl.setPrompt(prefix, prefix.length);
 rl.prompt();
